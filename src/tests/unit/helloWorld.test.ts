479afaa36c6d69c5d2f8e891ad3f56f37f05715e
Mark Herrmann <mark.herrmann@mailbox.org>
2023-02-08 20:21:07 +0100

    cleanup: renamings and preparings

--- src/tests/unit/helloWorld.test.ts
+++ src/tests/unit/helloWorld.test.ts
@@ -1,5 +1,5 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { lambdaHandler } from '../../app';
+import { lambdaHandler } from '../../helloWorld';
 
 describe('Unit test for app handler', function () {
     it('verifies successful response', async () => {
